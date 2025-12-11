1a014d450da0bb41fd9db33899902d1447647efb
madelily <madelily@naver.com>
2025-12-11 13:10:35 +0900

    lilylee_ 내정보 로그아웃버튼 추가

--- app/user-profile/[[...rest]]/page.tsx
+++ app/user-profile/[[...rest]]/page.tsx
@@ -1,10 +1,19 @@
 "use client";
-import { UserProfile } from "@clerk/nextjs";
+
+import { UserProfile, SignedIn, SignOutButton } from "@clerk/nextjs";
 
 export default function UserProfilePage() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="min-h-screen flex flex-col items-center justify-center gap-6">
       <UserProfile />
+
+      <SignedIn>
+        <SignOutButton redirectUrl="/sign-in">
+          <button className="px-4 py-2 bg-red-500 text-white rounded">
+            로그아웃
+          </button>
+        </SignOutButton>
+      </SignedIn>
     </div>
   );
 }
