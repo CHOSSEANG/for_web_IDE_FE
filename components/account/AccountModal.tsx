f77848628ea48fcfc413934cafc43e4a24667113
madelily <madelily@naver.com>
2025-12-18 16:10:20 +0900

    lilylee_뷰포트기준 user profile 상단 잘림 수정

--- components/account/AccountModal.tsx
+++ components/account/AccountModal.tsx
@@ -18,7 +18,11 @@ export default function AccountModal({
 }: AccountModalProps) {
   return (
     <Dialog open={open} onOpenChange={onClose}>
-      <DialogContent className="w-full mx-2 sm:mx-auto sm:max-w-[min(960px,65vw)]">
+      <DialogContent
+        className="w-full mx-2 top-4 -translate-y-0
+        sm:mx-auto sm:max-w-[min(960px,65vw)] sm:top-1/2 sm:-translate-y-1/2
+        max-h-[calc(100vh-1.5rem)] overflow-y-auto"
+      >
         <VisuallyHidden>
           <DialogTitle>계정 관리</DialogTitle>
         </VisuallyHidden>
