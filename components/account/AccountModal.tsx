7b95fea09e7e957272ed26b684677c25e0351283
madelily <madelily@naver.com>
2025-12-19 14:06:00 +0900

    lilylee_DialogContent 이슈 해결중

--- components/account/AccountModal.tsx
+++ components/account/AccountModal.tsx
@@ -1,10 +1,13 @@
-// @/componenets/account/AccountModal.tsx 
-// 회원 프로필 모달창 
+// @/components/account/AccountModal.tsx
+// 회원 프로필 모달창
 
 "use client";
 
-import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
-import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import AccountContent from "./AccountContent";
 
 type AccountModalProps = {
@@ -19,13 +22,18 @@ export default function AccountModal({
   return (
     <Dialog open={open} onOpenChange={onClose}>
       <DialogContent
-        className="w-full mx-2 top-4 -translate-y-0
-        sm:mx-auto sm:max-w-[min(960px,65vw)] sm:top-1/2 sm:-translate-y-1/2
-        max-h-[calc(100vh-1.5rem)] overflow-y-auto"
+        className="
+          w-full mx-2 top-4 -translate-y-0
+          sm:mx-auto sm:max-w-[min(960px,65vw)]
+          sm:top-1/2 sm:-translate-y-1/2
+          max-h-[calc(100vh-1.5rem)] overflow-y-auto
+        "
       >
-        <VisuallyHidden>
-          <DialogTitle>계정 관리</DialogTitle>
-        </VisuallyHidden>
+        {/* ✅ 접근성 필수: DialogTitle은 DialogContent의 직계 자식 */}
+        {/* ✅ 화면에는 보이지 않지만 DOM에는 존재 */}
+        <DialogTitle className="sr-only">
+          계정 관리
+        </DialogTitle>
 
         <AccountContent />
       </DialogContent>
