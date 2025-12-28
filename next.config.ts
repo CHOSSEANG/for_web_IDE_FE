63862dd1a81a4a5b4cfac65b18b6b4eb63914a5f
madelily <madelily@naver.com>
2025-12-29 01:08:11 +0900

    lilylee_fix(image): 허용되지 않은 외부 아바타 도메인으로 인한 이미지 로드 실패 수정

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.clerk.dev',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.gravatar.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   async rewrites() {
